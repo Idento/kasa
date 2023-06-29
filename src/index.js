062f3312c5fefbf78d2d64fc6433b114b9c64fd7
Idento <max.78.ch@gmail.com>
2023-06-29 13:24:09 +0200

    404, home, about done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.scss'
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import About from './components/about/About.js'
+import Failed from './components/Failed/Failed.js'
+import Loc from './components/Location/Loc'
+import Home, { navlist } from './components/home/home'
+import Header from './components/Header/Header'
+import Foot from './components/Footer/Foot'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+<BrowserRouter>
+	<React.StrictMode>
+		<Header navlist={navlist}/>
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/about' element={<About/>}/>
+			<Route path='/logement/:id' element={<Loc />}/>
+			<Route path='/*' element={<Failed />}/>
+		</Routes>
+		<Foot />
+	</React.StrictMode>
+</BrowserRouter>,
+	document.getElementById('root')
+)
